31ff917e67a3c426324cf44c4690720aee9b0322
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-07-20 14:04:07 +0200

    update

---
+++ Dev.ta.Application.Dto/DependecyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Dev.ta.Application.Dto
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddDto(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- Dev.ta/Program.cs
+++ Dev.ta/Program.cs
@@ -1,3 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using Dev.ta.Application.Dto;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+IConfiguration _configuration;
+var builder = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                        .AddEnvironmentVariables();
+_configuration = builder.Build();
 
